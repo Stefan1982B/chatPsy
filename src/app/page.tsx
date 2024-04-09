fa0446845d03e84ccf7cad30cf7112742607e0bd
Stefan1982B <stefanschuddinck@hotmail.com>
2024-04-09 07:53:26 +0200

    indigo

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -24,7 +24,7 @@ export default async function Home() {
   }
 
   return (
-    <div className="w-screen min-h-screen bg-gradient-to-r from-teal-100 to-teal-50">
+    <div className="w-screen min-h-screen bg-gradient-to-r from-indigo-50 to-indigo-300">
       <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2">
         <div className="flex flex-col items-center text-center">
           <div className="flex items-center">
